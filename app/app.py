c5b9febc2435a32190139fbb65e907aafbb03e72
Nishant Agrawal <nishantagrawal2003@gmail.com>
2025-12-08 02:28:30 +0530

    GET request

--- app/app.py
+++ app/app.py
@@ -1,7 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
+
+text_posts = {
+    1: {
+        "title": "Hello World",
+        "content": "This is a test post",
+        "author": "John Doe"
+    }
+}
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/posts")
+def read_posts():
+    return text_posts
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    if id not in text_posts:
+        raise HTTPException(status_code=404, detail="Post not found") 
+    return text_posts.get(id)
