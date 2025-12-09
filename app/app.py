dfcdb39f7a6e87534760b78b21bc79b27c3c8efe
Nishant Agrawal <nishantagrawal2003@gmail.com>
2025-12-09 18:56:30 +0530

    db working

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ from .schemas import PostCreate
 from .db import Post, create_db_and_tables, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
+from sqlalchemy import select
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -49,4 +50,35 @@ async def upload_file(
     caption: str = Form(...),
     session: AsyncSession = Depends(get_async_session)
 ):
-    pass
+    post = Post(
+        caption=caption,
+        url="dummy url",
+        file_type="photo",
+        file_name="dummy name"
+    )
+    session.add(post)
+    await session.commit()
+    await session.refresh(post)
+    return post
+
+@app.get("/feed")
+async def get_feed(
+    session: AsyncSession = Depends(get_async_session)
+):
+    results = await session.execute(select(Post).order_by(Post.created_at.desc()))
+    posts = [row[0] for row in results.all()]
+
+    posts_data = []
+    for post in posts:
+        posts_data.append(
+            {
+                "id": post.id,
+                "caption": post.caption,
+                "url": post.url,
+                "file_type": post.file_type,
+                "file_name": post.file_name,
+                "created_at": post.created_at.isoformat()
+            }
+        )
+    
+    return {"posts": posts_data}
