28482bf73e808fb666aa4fb53b8844b0a4eb5d3b
Nishant Agrawal <nishantagrawal2003@gmail.com>
2025-12-10 20:55:27 +0530

    user routes defined

--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,18 @@
 from pydantic import BaseModel
+from fastapi_users import schemas
+import uuid
 
 class PostCreate(BaseModel):
     title: str
     content: str
     author: str
+
+
+class UserRead(schemas.BaseUser[uuid.UUID]):
+    pass
+
+class UserCreate(schemas.BaseUserCreate):
+    pass
+
+class UserUpdate(schemas.BaseUserUpdate):
+    pass
--- app/users.py
+++ app/users.py
@@ -16,3 +16,36 @@ load_dotenv()
 
 SECRET = os.getenv("SECRET")
 
+class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
+    reset_password_token_secret = SECRET
+    verification_token_secret = SECRET
+
+    async def on_after_register(self, user: User, request: Optional[Request] = None):
+        print(f"User {user.id} has registered successfully.")
+
+    async def on_after_forgot_password(self, user: User, token: str, request: Optional[Request] = None):
+        print(f"User {user.id} has forgot password. Reset token: {token}")
+
+    async def on_after_request_verify(self, user: User, token: str, request: Optional[Request] = None):
+        print(f"User {user.id} has verified their email. Verification token: {token}")
+
+async def get_user_manager(user_db: SQLAlchemyUserDatabase = Depends(get_user_db)):
+    yield UserManager(user_db)
+
+bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
+
+def get_jwt_strategy() -> JWTStrategy:
+    return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
+
+auth_backend = AuthenticationBackend(
+    name="jwt",
+    transport=bearer_transport,
+    get_strategy=get_jwt_strategy,
+)
+
+fastapi_users = FastAPIUsers[User, uuid.UUID](
+    get_user_manager,
+    [auth_backend],
+)
+
+current_active_user = fastapi_users.current_user(active=True)
